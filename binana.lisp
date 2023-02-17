9b68876747464d61687d2dd5934fa03a61bb7597
Jan Deinhard <jan.deinhard@gmail.com>
2023-02-18 00:26:37 +0100

    show filetype

--- binana.lisp
+++ binana.lisp
@@ -2,6 +2,32 @@
 
 (in-package #:binana)
 
+(defconstant MH_OBJECT       1) ; relocatable object file
+(defconstant MH_EXECUTE      2) ; demand paged executable file
+(defconstant MH_FVMLIB       3) ; fixed VM shared library file
+(defconstant MH_CORE         4) ; core file
+(defconstant MH_PRELOAD      5) ; preloaded executable file
+(defconstant MH_DYLIB	     6) ; dynamically bound shared library
+(defconstant MH_DYLINKER     7) ; dynamic link editor
+(defconstant MH_BUNDLE	     8) ; dynamically bound bundle file
+(defconstant MH_DYLIB_STUB   9) ; shared library stub for static linking only, no section contents
+(defconstant MH_DSYM        10) ; companion file with only debug sections
+(defconstant MH_KEXT_BUNDLE 11) ; x86_64 kexts
+
+(defun map-to-filetype (value)
+  (cond ((= value MH_OBJECT) "Relocatable object file")
+	((= value MH_EXECUTE) "Executable file")
+	((= value MH_FVMLIB) "Fixed VM shared library file")
+	((= value MH_CORE) "Core file")
+	((= value MH_PRELOAD) "Preload executable file")
+	((= value MH_DYLIB) "Dynamically bound shared file")
+	((= value MH_DYLINKER) "Dynamic link editor")
+	((= value MH_BUNDLE) "Dynamically bound bundle file")
+	((= value MH_DYLIB_STUB) "Shared library stub for static linking")
+	((= value MH_DSYM) "Companinon file with only debug sections")
+	((= value MH_KEXT_BUNDLE) "x86-64 Kexts")
+	(t "Other")))
+
 (defun read-32-bit-word (input start)
   (logior (ash (aref input (+ start 3)) 24)
           (ash (aref input (+ start 2)) 16)
@@ -26,7 +52,7 @@
       (format t "magic      0x~d~%" (write-to-string magic :base 16))
       (format t "cputype    ~d~%" cputype)
       (format t "cpusubtype ~d~%" cpusubtype)
-      (format t "filetype   ~d~%" filetype)
+      (format t "filetype   ~d~%" (map-to-filetype filetype))
       (format t "ncmds      ~d~%" ncmds)
       (format t "sizeofcmds ~d~%" sizeofcmds)
       (format t "flags      0x~d~%" (write-to-string flags :base 16)))))
