422301d24c32eee123de5856791bba801387b054
Jan Deinhard <jan.deinhard@gmail.com>
2023-02-17 22:09:37 +0100

    remove obsolete commented code

--- binana.lisp
+++ binana.lisp
@@ -2,9 +2,6 @@
 
 (in-package #:binana)
 
-;    (let ((bytes (make-array 4 :initial-element 0)))
- ;     (read-sequence bytes stream :start start :end (+ start 4))
-
 (defun read-32-bit-word (input start)
   (logior (ash (aref input (+ start 3)) 24)
           (ash (aref input (+ start 2)) 16)
